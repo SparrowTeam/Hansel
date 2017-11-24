00bbd82e1beaa16f1fefe1408b78ed035f8a663a
Sergey Zobov <sergey@szobov.ru>
2017-11-25 00:18:54 +0300

    Добавил пакет.

---
+++ hansel/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages
+from setuptools.command.sdist import sdist
+
+
+__version__ = '0.0.1'
+
+setup(
+    name='hansel',
+    version=__version__,
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    entry_points={
+        'console_scripts': [
+            'hansel_run= hansel.start_application:start',
+        ]
+    },
+    install_requires=[
+        'flask',
+        'peewee',
+        'pyyaml'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+    ]
+)
